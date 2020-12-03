0c923c1d972ecbf2cdb509bf8dd28b9aca34eb87
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-03 22:52:35 +0300

    2020:12:03 22:52

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='applescript',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages()
 )
